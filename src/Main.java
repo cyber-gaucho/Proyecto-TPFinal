07a2460025cfbb7de62a5c2dd6824e35e69ed9b5
Valentino <valentinorenaudo@gmail.com>
2025-05-29 16:40:08 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola! Este es el Trabajo Practico Final de Programacion Concurrente 2025");
+    }
+}
